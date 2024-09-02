f242d1f61b28f6aa0449f6787faf951345320668
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-02 20:08:45 +0530

    Pattern-2

---
+++ patterns/pattern-2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Pattern-2
+// *
+// * *
+// * * *
+// * * * *
+void print(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    print(n);
+}
