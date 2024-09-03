866aaa51db5e55ba2b2ab52552b307b8e06a2789
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-03 16:21:41 +0530

    Pattern-10

---
+++ patterns/pattern-10.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Pattern-10
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+void print(int n)
+{
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = i;
+        if (i > n)
+        {
+            stars = 2 * n - i;
+        }
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
