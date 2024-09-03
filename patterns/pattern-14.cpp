c7985e6e76db16d0779f5f58ee003e0e6bf7e2cc
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-03 17:14:22 +0530

    Pattern-14

---
+++ patterns/pattern-14.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Pattern-14
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
+void print(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + i; ch++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
