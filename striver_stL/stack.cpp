8dcfaa986f6a68e019bb313f8f4f8011ea5ce95f
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-12 20:39:42 +0530

    Stack

---
+++ striver_stL/stack.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(3);
+    st.emplace(5);
+
+    cout << st.top(); // Prints 5 which is on top of stack
+    // Stack does not allow indexing to access data
+    st.pop();
+    cout << st.top();   // Prints 3
+    cout << st.empty(); // Prints 0 as false
+
+    stack<int> st1;
+    st.swap(st1);
+    // All operations are O(1)
+    return 0;
+}
