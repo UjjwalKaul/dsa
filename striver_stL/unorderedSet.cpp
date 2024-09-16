80e8f3ddb2946d4a4628535cfcec250dc3286d30
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-16 13:54:41 +0530

    Added unordered set

---
+++ striver_stL/unorderedSet.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_set<int> st; // Stores elements in unsorted order and accepts only unique values(no duplicates) rest same as set
+    // lower bound and upper bound functions do not work
+    return 0;
+}
